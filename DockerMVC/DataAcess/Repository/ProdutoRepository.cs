c4968ef577e43c8fab86a713d2aeb68681922aad
Angelo <angelo.knto@gmail.com>
2024-03-22 17:50:39 -0300

    ProdutoRepository implementado

--- DockerMVC/DataAcess/Repository/ProdutoRepository.cs
+++ DockerMVC/DataAcess/Repository/ProdutoRepository.cs
@@ -1,6 +1,17 @@
-﻿namespace DockerMVC.DataAcess.Repository
+﻿using DockerMVC.Models;
+
+namespace DockerMVC.DataAcess.Repository
 {
-    public class ProdutoRepository
+    public class ProdutoRepository : IProdutoRepository
     {
+        private static Produto[] produto = new Produto[]
+        {
+            new Produto {Id=1, Nome="Lápis", Categoria="Material", Quantidade=50, Preco=2.0M},
+            new Produto {Id=2, Nome="Caderno abc", Categoria="Caderno", Quantidade=30, Preco=25.0M},
+            new Produto {Id=3, Nome="Mochila azul", Categoria="Mochila", Quantidade=20, Preco=150.0M},
+            new Produto {Id=4, Nome="Borracha", Categoria="Material", Quantidade=50, Preco=1.5M},
+        };
+
+        public IEnumerable<Produto> ProdutoList { get => produto; }
     }
 }
--- DockerMVC/Models/Produto.cs
+++ DockerMVC/Models/Produto.cs
@@ -6,6 +6,6 @@
         public string Nome { get; set; }
         public string Categoria { get; set; }
         public int Quantidade { get; set; }
-        public int Preco { get; set; }
+        public decimal Preco { get; set; }
     }
 }
