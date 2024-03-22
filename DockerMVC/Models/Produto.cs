df534866053d577d2fa38159e002839499b531fc
Angelo <angelo.knto@gmail.com>
2024-03-22 17:38:55 -0300

    modelo Produto implementado

--- DockerMVC/Models/Produto.cs
+++ DockerMVC/Models/Produto.cs
@@ -2,5 +2,10 @@
 {
     public class Produto
     {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Categoria { get; set; }
+        public int Quantidade { get; set; }
+        public int Preco { get; set; }
     }
 }
