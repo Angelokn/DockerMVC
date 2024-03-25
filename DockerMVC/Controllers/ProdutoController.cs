7e4a9129960a2a8b7f05caa23e323c4f606bf4e9
Angelo <angelo.knto@gmail.com>
2024-03-25 19:43:46 -0300

    injected dependencies in ProdutoController

--- DockerMVC/Controllers/ProdutoController.cs
+++ DockerMVC/Controllers/ProdutoController.cs
@@ -1,9 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DockerMVC.DataAcess.Repository;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DockerMVC.Controllers
 {
     public class ProdutoController : Controller
     {
+        private IProdutoRepository _produtoRepository;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public ProdutoController(IProdutoRepository produtoRepository, 
+            IHttpContextAccessor httpContextAccessor)
+        {
+            _produtoRepository = produtoRepository;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
         public IActionResult Index()
         {
             return View();
