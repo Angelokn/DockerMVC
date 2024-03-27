c7c7c8b4b9e3b8a1590ef66bb338ec0c26e3352c
Angelo <angelo.knto@gmail.com>
2024-03-27 17:25:11 -0300

    created AppDbContext

---
+++ DockerMVC/DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using DockerMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DockerMVC.DataAccess
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
