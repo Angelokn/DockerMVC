83afe932ec0f880cadff34e8d7588b6ace28294e
Angelo <angelo.knto@gmail.com>
2024-03-27 17:28:46 -0300

    updated ProdutoRepository to use db

--- DockerMVC/DataAccess/Repository/ProdutoRepository.cs
+++ DockerMVC/DataAccess/Repository/ProdutoRepository.cs
@@ -1,17 +1,28 @@
-﻿using DockerMVC.Models;
+﻿using DockerMVC.DataAccess;
+using DockerMVC.Models;
 
 namespace DockerMVC.DataAcess.Repository
 {
     public class ProdutoRepository : IProdutoRepository
     {
-        private static Produto[] produtos = new Produto[]
-        {
-            new Produto {Id=1, Nome="Lápis", Categoria="Material", Quantidade=50, Preco=2.0M},
-            new Produto {Id=2, Nome="Caderno abc", Categoria="Caderno", Quantidade=30, Preco=25.0M},
-            new Produto {Id=3, Nome="Mochila azul", Categoria="Mochila", Quantidade=20, Preco=150.0M},
-            new Produto {Id=4, Nome="Borracha", Categoria="Material", Quantidade=50, Preco=1.5M},
-        };
+        private ApplicationDbContext _db;
 
-        public IEnumerable<Produto> Produtos { get => produtos; }
+        public ProdutoRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<Produto> Produtos => _db.Produtos;
+
+
+        //private static Produto[] produtos = new Produto[]
+        //{
+        //    new Produto {Id=1, Nome="Lápis", Categoria="Material", Quantidade=50, Preco=2.0M},
+        //    new Produto {Id=2, Nome="Caderno abc", Categoria="Caderno", Quantidade=30, Preco=25.0M},
+        //    new Produto {Id=3, Nome="Mochila azul", Categoria="Mochila", Quantidade=20, Preco=150.0M},
+        //    new Produto {Id=4, Nome="Borracha", Categoria="Material", Quantidade=50, Preco=1.5M},
+        //};
+
+        //public IEnumerable<Produto> Produtos { get => produtos; }
     }
 }
