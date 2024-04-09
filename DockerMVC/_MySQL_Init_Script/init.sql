fbdc49b83d679c3c3992946c74b1e9a984cc176d
Angelo <angelo.knto@gmail.com>
2024-04-09 19:29:32 -0300

    updated init.sql

--- DockerMVC/_MySQL_Init_Script/init.sql
+++ DockerMVC/_MySQL_Init_Script/init.sql
@@ -1,1 +1,20 @@
-﻿
+﻿CREATE DATABASE produtosbd;
+
+USE produtosdb;
+
+DROP TABLE IF EXISTS Produtos;
+
+CREATE TABLE Produtos (
+	Id INT AUTO_INCREMENT,
+	Nome VARCHAR(30) NOT NULL,
+	Categoria VARCHAR(30) NOT NULL,
+	Quantidade INT NOT NULL,
+	Preco DECIMAL(10,2) NOT NULL,
+	PRIMARY KEY (Id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES Produtos WRITE;
+INSERT INTO Produtos VALUES(1,"Lápis","Material", 50, "2.0");
+INSERT INTO Produtos VALUES(2, "Caderno 55", "Caderno", 30, "25.0");
+INSERT INTO Produtos VALUES(3, "Mochila verde", "Mochila", 20, "150.0");
+UNLOCK TABLES;
